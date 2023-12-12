f7d63e2351e3ff1c4ed739f3ac5085381c0aca5d
Murad <gadjiev@lumabit.de>
2023-12-12 12:53:56 +0100

    refactor login and signup

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,25 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC4WhITKrTwNJ8-4HrBnM-52IgvfS8qmM4', {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        })
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC4WhITKrTwNJ8-4HrBnM-52IgvfS8qmM4';
+
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4WhITKrTwNJ8-4HrBnM-52IgvfS8qmM4';
+        }
+
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -21,33 +40,9 @@ export default {
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         })
-
     },
-    async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4WhITKrTwNJ8-4HrBnM-52IgvfS8qmM4', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        })
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            const error = new Error(responseData.message || 'Failed to authenticate');
-            throw error;
-        }
-
+    logout(context) {
         context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
-        })
-    },
-    logout(context){
-        context.commit('setUser',{
             token: null,
             userId: null,
             tokenExpiration: null
