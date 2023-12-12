783a782871a091cd765d0d1fcd18b3a492e401d7
Murad <gadjiev@lumabit.de>
2023-12-12 13:44:57 +0100

    Auto Login & Logout

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -38,7 +38,7 @@ export default {
             return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
         },
         contactLink() {
-            return this.$route.path + '/' + this.id + '/contact';
+            return '/coaches/' + this.id + '/contact';
         },
         areas() {
             return this.selectedCoach.areas
